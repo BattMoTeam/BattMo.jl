2c5cd892d2e34939fb1e9d5963bbb824f48336b6
Olav Møyner <olav.moyner@gmail.com>
2022-11-01 19:31:22 +0100

    Add general AD to tests

--- test/battery/test_battery_boundary.jl
+++ test/battery/test_battery_boundary.jl
@@ -24,19 +24,21 @@ allfine = Vector{Bool}();
 @testset "battery" begin
     for (modelname, max_step) in testcases
         @testset "$modelname" begin
-            states, report, extra = test_battery(modelname, max_step = max_step, info_level = -1);
-            stateref = extra[:states_ref]
-            steps = size(states, 1)
-            E = Matrix{Float64}(undef,steps,2)
-            for step in 1:steps
-                phi = states[step][:BPP][:Phi][1]
-                E[step,1] = phi
-                phi_ref = stateref[step]["Control"]["E"]
-                E[step,2] = phi_ref
+            for use_general_ad in [false, true]
+                states, report, extra = test_battery(modelname, max_step = max_step, info_level = -1, general_ad = use_general_ad);
+                stateref = extra[:states_ref]
+                steps = size(states, 1)
+                E = Matrix{Float64}(undef,steps,2)
+                for step in 1:steps
+                    phi = states[step][:BPP][:Phi][1]
+                    E[step,1] = phi
+                    phi_ref = stateref[step]["Control"]["E"]
+                    E[step,2] = phi_ref
+                end
+                #append!(allfine,steps == 65)
+                #append!(allfine,all(abs.(E[:,1]-E[:,2])./E[:,1] .< 0.1))
+                @test isapprox(E[:, 1], E[:, 2], rtol = 0.1)
             end
-            #append!(allfine,steps == 65)
-            #append!(allfine,all(abs.(E[:,1]-E[:,2])./E[:,1] .< 0.1))
-            @test isapprox(E[:, 1], E[:, 2], rtol = 0.1)
         end
     end
 end
