958ab3b7b2babcbc949fa58a1e5d1cc7f834d88b
Xavier Raynaud <xav.raynaud@gmail.com>
2023-03-29 14:17:13 +0200

    clean-up for ocp

--- src/models/ocp.jl
+++ src/models/ocp.jl
@@ -1,9 +1,9 @@
-export Grafite, NMC111, ocd, n_charge_carriers, cMax, volumetric_surface_area
+export Graphite, NMC111, ocp, n_charge_carriers, volumetric_surface_area
 
-struct Grafite <: ActiveMaterial end
+struct Graphite <: ActiveMaterial end
 struct NMC111 <: ActiveMaterial end
 
-
+## Define OCP and entropy change (dUdT) for graphite using polynomials
 
 const coeff1_refOCP = Polynomial([
 	-4.656,
@@ -48,21 +48,21 @@ const coeff2_dUdT = Polynomial([
 	+ 3.048755063
 ])
 
-function ocd(T,c, material::NMC111)
-
-	refT = 298.15
-	#T=300
-	#c=0.1
-	#D0: 1.0000e-14
-	#EaD: 5000
-    cmax = maximum_concentration(material)
-	theta = c/cmax
-	refOCP = coeff1_refOCP(theta)/coeff2_refOCP(theta)
-	dUdT = -1e-3*coeff1_dUdT(theta)/coeff2_dUdT(theta)
-	vocd = refOCP + (T - refT) * dUdT
-	return vocd
+function ocp(T,c, material::NMC111)
+    """Compute OCP for NMC111 as function of temperature and concentration"""
+    refT   = 298.15
+    cmax   = maximum_concentration(material)
+    theta  = c/cmax
+    refOCP = coeff1_refOCP(theta)/coeff2_refOCP(theta)
+    dUdT   = -1e-3*coeff1_dUdT(theta)/coeff2_dUdT(theta)
+    vocp   = refOCP + (T - refT) * dUdT
+    
+    return vocp
+    
 end
-##
+
+## Defines OCP and entropy change (dUdT) for graphite using polynomials
+
 const coeff1 = Polynomial([
 	+ 0.005269056,
 	+ 3.299265709,
@@ -87,78 +87,86 @@ const  coeff2= Polynomial([
 	+ 165705.8597
 ]);
 
-function ocd(T,c, material::Grafite)
-    cmax = maximum_concentration(material)
-	# EaD: 5000
-	# D0: 3.9000e-14
-  	# cmax: 30555
-    theta = c./cmax
-    refT = 298.15
-    refOCP = (
-		0.7222
-        + 0.1387 * theta
-        + 0.0290 * theta^0.5
-        - 0.0172 / theta
-        + 0.0019 / theta^1.5
-        + 0.2808 * exp(0.9 - 15.0*theta)
-        - 0.7984 * exp(0.4465*theta - 0.4108)
-		);
+function ocp(T, c, material::Graphite)
+    """Compute OCP for Graphite as function of temperature and concentration"""
+    cmax   = maximum_concentration(material)
+    theta  = c./cmax
+    refT   = 298.15
+    refOCP = (0.7222
+              + 0.1387 * theta
+              + 0.0290 * theta^0.5
+              - 0.0172 / theta
+              + 0.0019 / theta^1.5
+              + 0.2808 * exp(0.9 - 15.0*theta)
+              - 0.7984 * exp(0.4465*theta - 0.4108)
+	      );
 
-	dUdT = 1e-3*coeff1(theta)/ coeff2(theta);
-
-	vocd = refOCP + (T - refT) * dUdT;
-	return vocd
+    dUdT = 1e-3*coeff1(theta)/ coeff2(theta);
+    
+    vocp = refOCP + (T - refT) * dUdT;
+    return vocp
+    
 end
 
-function n_charge_carriers(::Grafite)
-        return 1
+function n_charge_carriers(::Graphite)
+    return 1
 end
 
 function n_charge_carriers(::NMC111)
-        return 1
+    return 1
 end
 
-maximum_concentration(::Grafite) = 30555.0
-maximum_concentration(::NMC111) = 55554.0
+maximum_concentration(::Graphite) = 30555.0
+maximum_concentration(::NMC111)   = 55554.0
 
-volumetric_surface_area(::Grafite) =723600.0
-volumetric_surface_area(::NMC111) = 885000.0
+volumetric_surface_area(::Graphite) = 723600.0
+volumetric_surface_area(::NMC111)   = 885000.0
 
-function reaction_rate_const(T, c, ::Grafite)
-	refT = 298.15
-	k0 = 5.0310e-11
-	Eak = 5000
-	val = k0.*exp(-Eak./FARADAY_CONST .*(1.0./T - 1/refT));
-	return val
+## Defines exchange current density for Graphite
+
+function reaction_rate_const(T, c, ::Graphite)
+
+    refT = 298.15
+    k0   = 5.0310e-11
+    Eak  = 5000
+    val  = k0.*exp(-Eak./FARADAY_CONST .*(1.0./T - 1/refT));
+    
+    return val
+    
 end
+
+## Defines exchange current density for NMC
 
 function reaction_rate_const(T, c, ::NMC111)
-	refT = 298.15
-	k0 = 2.3300e-11
-	Eak = 5000
-	val = k0.*exp(-Eak./FARADAY_CONST .*(1.0./T - 1/refT));
-	return val
+    refT = 298.15
+    k0 = 2.3300e-11
+    Eak = 5000
+    val = k0.*exp(-Eak./FARADAY_CONST .*(1.0./T - 1/refT));
+    
+    return val
 end
 
+## Define solid diffusion coefficient for Graphite
 
-function diffusion_rate(T,c,::Grafite)
-	refT = 298.15
-	D0 = 3.900000000000000e-14
-	Ead = 5000
-	val = D0.*exp(-Ead./FARADAY_CONST .*(1.0./T - 1/refT));
-	return val
-end
-function diffusion_rate(T,c,::NMC111)
-	refT = 298.15
-	D0 = 1.000000000000000e-14
-	Ead = 5000
-	val = D0.*exp(-Ead./FARADAY_CONST .*(1.0./T - 1/refT));
-	return val
+function diffusion_rate(T, c, ::Graphite)
+    
+    refT = 298.15
+    D0   = 3.9e-14
+    Ead  = 5000
+    val  = D0.*exp(-Ead./FARADAY_CONST .*(1.0./T - 1/refT));
+    
+    return val
 end
 
-##
-#grafite = Grafite()
-#nmc111 = NMC111()
-#T=300;
-#c=0.1;
-#a=OCD(T,c,nmc111)
+## Define solid diffusion coefficient for NMC
+
+function diffusion_rate(T, c, ::NMC111)
+    
+    refT = 298.15
+    D0   = 1e-14
+    Ead  = 5000
+    val = D0.*exp(-Ead./FARADAY_CONST .*(1.0./T - 1/refT));
+    
+    return val
+end
+
