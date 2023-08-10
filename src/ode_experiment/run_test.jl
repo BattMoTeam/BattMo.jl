d277f2d2310c6bfa79998d13b158d08cc37de086
Erasdna <solheim.andreas@gmail.com>
2023-08-10 16:20:44 +0200

    start testing

--- src/ode_experiment/run_test.jl
+++ src/ode_experiment/run_test.jl
@@ -1,6 +1,9 @@
-import BattMo, DifferentialEquations
+using BattMo, DifferentialEquations, Jutul, Sundials, DiffEqBase
 
-init = BattMo.MatlabFile()
+include("state_variable_mapping.jl")
+
+#init = BattMo.MatlabFile("/home/andreas/SINTEF/json_experiment/BattMo.jl/test/battery/data/model1D_50.mat")
+init = JSONFile("/home/andreas/SINTEF/json_experiment/BattMo.jl/test/battery/data/jsonfiles/p2d_40_jl.json")
 use_p2d=false
 use_groups=false
 extra_timing=false
@@ -10,15 +13,43 @@ general_ad=false
 use_groups=false
 
 #Setup simulation
-sim, forces, state0, parameters, init, model = setup_sim(init, use_p2d=use_p2d, use_groups=use_groups, general_ad=general_ad)
+sim, forces, state0, parameters, init, model = BattMo.setup_sim(init, use_p2d=use_p2d, use_groups=use_groups, general_ad=general_ad)
 
 #Set up config and timesteps
-timesteps=setup_timesteps(init;max_step=max_step)
-cfg=setup_config(sim,model,linear_solver,extra_timing)
+timesteps=BattMo.setup_timesteps(init;max_step=max_step)
+cfg=BattMo.setup_config(sim,model,linear_solver,extra_timing)
 
-#Perform simulation
-f!(dy,y,p,t) = odeFun!(dy,y,p,y,model,state,forces,config)
 
-y0 = stateToVariable(state0,model)
 
-problem = DifferentialEquations.DAEProblem(f!,y0,timesteps)
+y0 = stateToVariable(sim.storage[:state],model)
+#variableToState!(state0,y0,model)
+
+#sim.storage[:state]=state0
+r=odeFun!(y0,y0,nothing,0,model,sim,forces)
+
+f!(dy,y,p,t) = odeFun!(dy,y,p,t,model,sim,forces)
+
+tspan = (0.0, 5000)
+
+#DAE
+#prob=DAEProblem(f!,y0*.0,y0,tspan,differential_vars=(ones(size(y0)).!=1.0))
+#solve(prob)
+
+#ODE 
+prob=ODEProblem(f,y0,tspan)
+solve(prob,Rosenbrock23(autodiff=false))
+
+#Q: Modify sim.storage[:state] directly?
+#println(state0[:BPP])
+#println("Storage state")
+#println(sim.storage[:state][:BPP])
+#st=sim.storage[:state]
+#variableToState!(sim.storage[:state],y0,model)
+#println("modified")
+#println(st)
+
+#println(y0)
+
+#r = odeFun!(y0,y0,nothing,0,model,sim,forces)
+#println(r)
+#problem = DifferentialEquations.DAEProblem(f!,y0,timesteps)
--- src/ode_experiment/state_variable_mapping.jl
+++ src/ode_experiment/state_variable_mapping.jl
@@ -1,8 +1,7 @@
 import Jutul
-
-#Genereate vector of primary variables from a state
+#Generate vector of primary variables from a state
 function stateToVariable(state,model::Jutul.MultiModel)
-    mapper,=Jutul.variable_mapper(model) #primary variables mappper
+    mapper,=Jutul.variable_mapper(model,:primary) #primary variables mappper
     #Vectorize primary variables collected with mapper
     variable=Jutul.vectorize_variables(model,state,mapper)
     return variable
@@ -10,28 +9,64 @@ end
 
 #Recover a state from vector of primary variables
 function variableToState!(state,variable,model::Jutul.MultiModel)
-    mapper,=Jutul.variable_mapper(model)
+    mapper,=Jutul.variable_mapper(model,:primary)
     Jutul.devectorize_variables!(state,model,variable,mapper)
 end
 
-function odeFun!(dy,y,p,t,model,storage,forces,config)
-    #Change state in storage
+function odeFun!(dy,y,p,t,model,sim,forces)
     
-    state=variableToState!(state,y,model)
+    #Change state in storage
+    variableToState!(sim.storage[:state],y,model)
+    state0 =sim.storage[:state0]
+    state= sim.storage[:state]
 
     dt=1 # Dummy 
-    #Update time somehow?
+    update_secondary=true
 
+    #Source + div terms for state
+    sim.storage[:state0]=state
+    Jutul.update_state_dependents!(sim.storage, model, dt, forces, time = t, update_secondary = update_secondary)
+    Jutul.update_linearized_system!(sim.storage, model, sim.executor)
+    eq1 = sim.storage.LinearizedSystem.r
 
-    #Time?
-    #update_state_dependents!(storage, model, dt, forces, time = time, update_secondary = update_secondary)
-    #update_linearized_system!(storage, model, executor)
-    
-    #Update eqations etc with perform step!
-    #Q: Difference between state and storage? Conversion
-    perform_step!(state,model,dt,forces,config; solve=false)
+    #Source + div terms for state0
+    sim.storage[:state]=state0
+    sim.storage[:state0]=state0
+    Jutul.update_state_dependents!(sim.storage, model, dt, forces, time = t, update_secondary = update_secondary)
+    Jutul.update_linearized_system!(sim.storage, model, sim.executor)
+    eq10 = sim.storage.LinearizedSystem.r
 
-    #Now extract state.LinearizedSystem.r somehow?
-    #Q: What type is storage.LinearizedSystem.r? Use some conversion... 
-    
+    #Create an empty state
+    null_state=empty_state(sim.storage[:state])
+    sim.storage[:state0]=null_state
+
+    #Calculate M(state):
+    sim.storage[:state]=state
+    Jutul.update_state_dependents!(sim.storage, model, dt, forces, time = t, update_secondary = update_secondary)
+    Jutul.update_linearized_system!(sim.storage, model, sim.executor)
+    eq2 = (sim.storage.LinearizedSystem.r - eq1)*dt
+    #Calculate M(state0)
+    #Update time somehow?    
+    sim.storage[:state]=state0
+    Jutul.update_state_dependents!(sim.storage, model, dt, forces, time = t, update_secondary = update_secondary)
+    Jutul.update_linearized_system!(sim.storage, model, sim.executor)
+    eq3 = (sim.storage.LinearizedSystem.r - eq10)*dt
+
+    accum = (eq2-eq3)/dt
+    println(eq1 +accum)
+    return eq1 + accum
+end
+
+function empty_state(example_state)
+    ret=example_state
+    for (k,v) in pairs(example_state)
+        if typeof(ret) <: Jutul.JutulStorage
+            ret[k]=empty_state(v)
+        elseif typeof(ret) <: NamedTuple
+            if typeof(v) <: Vector 
+                ret=merge(ret,[k=>v.*0])
+            end 
+        end
+    end
+    return ret
 end
