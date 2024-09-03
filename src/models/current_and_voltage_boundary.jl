d11feb333b576b0d9bc103b0633acb55f9284aab
Xavier Raynaud <xav.raynaud@gmail.com>
2024-09-03 15:42:31 +0200

    minor clean-up

--- src/models/current_and_voltage_boundary.jl
+++ src/models/current_and_voltage_boundary.jl
@@ -730,22 +730,24 @@ function sineup(y1::T, y2::T, x1::T, x2::T, x::T) where {T<:Any}
     #   res = sineup(y1, y2, x1, x2, x) creates a sine ramp
     #   starting at value y1 at point x1 and ramping to value y2 at
     #   point x2 over the vector x.
-        
-        dy = y1 - y2; 
-        dx = abs(x1 - x2);
-        res::T = 0.0 
-         if  (x >= x1) && (x <= x2)
-            res = dy/2.0.*cos(pi.*(x - x1)./dx) + y1 - (dy/2) 
-        end
-        
-        if (x > x2)
-            res .+= y2
-        end
-
-        if (x < x1)
-            res .+= y1
-        end
     
-        return res
+    dy = y1 - y2; 
+    dx = abs(x1 - x2);
+    
+    res::T = 0.0
+    
+    if  (x >= x1) && (x <= x2)
+        res = dy/2.0.*cos(pi.*(x - x1)./dx) + y1 - (dy/2) 
+    end
+    
+    if (x > x2)
+        res .+= y2
+    end
+
+    if (x < x1)
+        res .+= y1
+    end
+    
+    return res
     
 end
