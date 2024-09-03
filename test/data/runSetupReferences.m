0959d5780b73419ec08cf84dde5d6bd02c14bc17
Xavier Raynaud <xav.raynaud@gmail.com>
2024-09-03 17:00:43 +0200

    update in matlab runSetupReferences function

--- test/data/runSetupReferences.m
+++ test/data/runSetupReferences.m
@@ -1,20 +1,18 @@
-% casenames = {'p2d_40',
-             % '3d_demo_case'};
-
-casenames = {'p2d_40_cccv'};
-% casenames = {'p2d_40'};
+casenames = {'p2d_40_no_cc',
+             'p2d_40',
+             '3d_demo_case'};
 
 battmo_folder = fileparts(mfilename('fullpath'));
-battmo_folder = fullfile(battmo_folder, '../../..');
+battmo_folder = fullfile(battmo_folder, '../..');
 
 if exist('setupMatlabReference') == 0
     % we add the setup function in matlab path
-    import_folder = fullfile(battmo_folder, 'src/utils/');
+    import_folder = fullfile(battmo_folder, 'src/matlab_interface/');
     addpath(import_folder)
 end
 
-jsonfolder = fullfile(battmo_folder, 'test/battery/data/jsonfiles/');
-datafolder = fullfile(battmo_folder, 'test/battery/data');
+jsonfolder = fullfile(battmo_folder, 'test/data/jsonfiles/');
+datafolder = fullfile(battmo_folder, 'test/data/matlab_files');
 
 for icase = 1 : numel(casenames)
     casename = casenames{icase};
