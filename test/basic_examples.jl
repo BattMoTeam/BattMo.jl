89191229b4ac53e62f028f667ed2d9125da268ee
Xavier Raynaud <xavier.raynaud@sintef.no>
2024-09-04 11:43:40 +0200

    rewrote basic tests

--- test/basic_examples.jl
+++ test/basic_examples.jl
@@ -1,27 +1,32 @@
 using BattMo
 using Test
 
-testcases  =[
+names  =[
     "p2d_40",
-    "3d_demo_case"
-    ]
+    "p2d_40_jl_chen2020",
+    "p2d_40_jl_ud_func",
+    "p2d_40_jl_ud_tab",
+    "p2d_40_no_cc",
+    "p2d_40_cccv"
+]
 
-@testset "battery" begin
-    for modelname in testcases
-        @testset "$modelname" begin
-            for use_general_ad in [false, true]
-                states, report, extra = run_battery(modelname, info_level = -1, general_ad = use_general_ad);
-                stateref = extra[:init].object["states"]
-                steps = size(states, 1)
-                E = Matrix{Float64}(undef, steps, 2)
-                for step in 1:steps
-                    phi = states[step][:Control][:Phi][1]
-                    E[step, 1] = phi
-                    phi_ref = stateref[step]["Control"]["E"]
-                    E[step, 2] = phi_ref
-                end
-                @test isapprox(E[:, 1], E[:, 2], rtol = 0.1)
+names = ["p2d_40_no_cc"]
+
+@testset  begin
+    for name in names
+        @test begin
+            fn = string(dirname(pathof(BattMo)), "/../test/data/jsonfiles/", name, ".json")
+            inputparams = readBattMoJsonInputFile(fn)
+            function hook(simulator,
+                          model,
+                          state0,
+                          forces,
+                          timesteps,
+                          cfg)
+                cfg[:error_on_incomplete] = true
             end
+            output = run_battery(inputparams; hook = hook)
+            true
         end
     end
 end
