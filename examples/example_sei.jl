551c666a9e4fc3d4dd537185ee1a2eeffe79b41f
Xavier Raynaud <xav.raynaud@gmail.com>
2024-09-27 16:19:47 +0200

    added example script for sei

---
+++ examples/example_sei.jl
@@ -0,0 +1,82 @@
+#=
+Electro-Chemical component
+A component with electric potential, concentration and temperature
+The different potentials are independent (diagonal onsager matrix),
+and conductivity, diffusivity is constant.
+=#
+
+using Jutul, BattMo, GLMakie
+
+
+name = "bolay"
+fn = string(dirname(pathof(BattMo)), "/../test/data/jsonfiles/", name, ".json")
+inputparams = readBattMoJsonInputFile(fn)
+config_kwargs = (info_level = 10, )
+
+function hook(simulator,
+              model,
+              state0,
+              forces,
+              timesteps,
+              cfg)
+    # cfg[:max_timestep_cuts] = 1
+    # cfg[:max_nonlinear_iterations] = 10
+end
+
+output = run_battery(inputparams;
+                     hook = hook,
+                     config_kwargs = config_kwargs,
+                     extra_timing = false);
+
+states = output[:states]
+
+t = [state[:Control][:ControllerCV].time for state in states]
+E = [state[:Control][:Phi][1] for state in states]
+I = [state[:Control][:Current][1] for state in states]
+
+f = Figure(size = (1000, 400))
+
+ax = Axis(f[1, 1],
+          title     = "Voltage",
+          xlabel    = "Time / s",
+          ylabel    = "Voltage / V",
+          xlabelsize = 25,
+          ylabelsize = 25,
+          xticklabelsize = 25,
+          yticklabelsize = 25
+          )
+
+scatterlines!(ax,
+              t,
+              E;
+              linewidth = 4,
+              markersize = 10,
+              marker = :cross, 
+              markercolor = :black,
+              label = "Julia"
+              )
+
+ax = Axis(f[1, 2],
+          title     = "Current",
+          xlabel    = "Time / s",
+          ylabel    = "Current / A",
+          xlabelsize = 25,
+          ylabelsize = 25,
+          xticklabelsize = 25,
+          yticklabelsize = 25
+          )
+
+scatterlines!(ax,
+              t,
+              I;
+              linewidth = 4,
+              markersize = 10,
+              marker = :cross, 
+              markercolor = :black,
+              label = "Julia"
+              )
+
+display(f)
+
+
+
