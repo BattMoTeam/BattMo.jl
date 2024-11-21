da47080f3fcb6af46d6abb27908054a12adc2133
Xavier Raynaud <xav.raynaud@gmail.com>
2024-11-21 15:08:58 +0100

    more in flux reconstruction

--- src/utils/assembly.jl
+++ src/utils/assembly.jl
@@ -1,3 +1,63 @@
+export fluxReconstruction
+
+function fluxReconstruction(model, state, parameters, operators)
+    
+    storage = Jutul.setup_storage(model;
+                                  setup_linearized_system = false,
+                                  setup_equations         = false,
+                                  state0                  = state,
+                                  state_ad                = false,
+                                  parameters              = parameters)
+    
+    storage = convert_to_immutable_storage(storage)
+    Jutul.update_secondary_variables!(storage, model)
+
+    state = storage[:state]
+    
+    domain = model.domain.representation
+
+    trans = domain[:trans]
+
+    g = domain.representation
+    
+    cfmap     = g.faces.cells_to_faces
+    neighbors = g.faces.neighbors
+
+    nc = Jutul.number_of_cells(g)
+
+    fluxVector = Array{Float64}(undef, 3, nc)
+    
+    for cell in 1 : nc
+        
+        cface_inds = cfmap.pos[cell] : (cfmap.pos[cell + 1] - 1)
+        cface_inds = cfmap.vals[cface_inds]
+
+        localflux = Vector{Float64}(undef, length(cface_inds))
+        
+        for (iloc, iface) in enumerate(cface_inds)
+            if cell == neighbors[iface][1]
+                other_cell = neighbors[iface][2]
+                face_sign = 1
+            else
+                other_cell = neighbors[iface][1]
+                face_sign = 2
+            end
+            
+            localflux[iloc] = computeFlux(Val(:Charge), model, state, cell, other_cell, iface)
+            
+        end
+
+        op = operators[cell]
+
+        fluxVector[:, cell] = op*localflux
+        
+    end
+
+    return fluxVector
+    
+end
+
+
 function setupFluxOperator(domain::DataDomain{R, <:Any, <:Any}) where {R <: UnstructuredMesh}
     
     g = domain.representation
@@ -6,7 +66,6 @@ function setupFluxOperator(domain::DataDomain{R, <:Any, <:Any}) where {R <: Unst
     neighbors = g.faces.neighbors
     
     nc = Jutul.number_of_cells(g)
-    nf = Jutul.number_of_faces(g)
 
     # normals array (size dim x nf) 
     normals = domain[:normals]
--- src/utils/test_assembly.jl
+++ src/utils/test_assembly.jl
@@ -1,4 +1,32 @@
+using Jutul, BattMo, GLMakie
+
+doinit = false
+
+if doinit
+    
+    # ## Setup input parameters
+    name = "p2d_40_jl_chen2020"
+
+    fn = string(dirname(pathof(BattMo)), "/../test/data/jsonfiles/", name, ".json")
+    inputparams = readBattMoJsonInputFile(fn)
+
+    fn = string(dirname(pathof(BattMo)), "/../test/data/jsonfiles/3d_demo_geometry.json")
+    inputparams_geometry = readBattMoJsonInputFile(fn)
+
+    inputparams = mergeInputParams(inputparams_geometry, inputparams)
+
+    # ## Setup and run simulation
+
+    output = run_battery(inputparams);
+
+    return
+    
+end
+
+model      = output[:extra][:model][:Elyte]
+state      = output[:states][end][:Elyte]
+parameters = output[:extra][:parameters][:Elyte]
+
 operators = BattMo.setupFluxOperator(model.domain.representation)
 
-a = [size(op) for op in operators]
-
+fluxVector = fluxReconstruction(model, state, parameters, operators)
