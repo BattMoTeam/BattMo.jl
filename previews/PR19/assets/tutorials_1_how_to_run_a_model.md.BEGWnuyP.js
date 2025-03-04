5c9c645d55fcbe3eaa03107280c9bca7be8dafd5
Documenter.jl <documenter@juliadocs.github.io>
2025-03-04 10:24:04 +0000

    build based on f4860fc

---
+++ previews/PR19/assets/tutorials_1_how_to_run_a_model.md.BEGWnuyP.js
@@ -0,0 +1,18 @@
+import{_ as n,c as s,o as e,aA as t}from"./chunks/framework.BIuidntl.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"tutorials/1_how_to_run_a_model.md","filePath":"tutorials/1_how_to_run_a_model.md","lastUpdated":null}'),o={name:"tutorials/1_how_to_run_a_model.md"};function p(l,a,r,i,u,_){return e(),s("div",null,a[0]||(a[0]=[t(`<div class="language-@example vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">@example</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line highlighted"><span>{</span></span>
+<span class="line"><span> &quot;cells&quot;: [</span></span>
+<span class="line"><span>  {</span></span>
+<span class="line"><span>   &quot;cell_type&quot;: &quot;markdown&quot;,</span></span>
+<span class="line"><span>   &quot;metadata&quot;: {},</span></span>
+<span class="line"><span>   &quot;source&quot;: [</span></span>
+<span class="line"><span>    &quot;## Tutorial 1 - How to run a model&quot;</span></span>
+<span class="line"><span>   ]</span></span>
+<span class="line"><span>  }</span></span>
+<span class="line"><span> ],</span></span>
+<span class="line"><span> &quot;metadata&quot;: {</span></span>
+<span class="line"><span>  &quot;language_info&quot;: {</span></span>
+<span class="line"><span>   &quot;name&quot;: &quot;python&quot;</span></span>
+<span class="line"><span>  }</span></span>
+<span class="line"><span> },</span></span>
+<span class="line"><span> &quot;nbformat&quot;: 4,</span></span>
+<span class="line"><span> &quot;nbformat_minor&quot;: 2</span></span>
+<span class="line"><span>}</span></span></code></pre></div><h2 id="Example-on-GitHub" tabindex="-1">Example on GitHub <a class="header-anchor" href="#Example-on-GitHub" aria-label="Permalink to &quot;Example on GitHub {#Example-on-GitHub}&quot;">​</a></h2><p>If you would like to run this example yourself, it can be downloaded from the BattMo.jl GitHub repository <a href="https://github.com/BattMoTeam/BattMo.jl/blob/main/examples/1_how_to_run_a_model.jl" target="_blank" rel="noreferrer">as a script</a>, or as a <a href="https://github.com/BattMoTeam/BattMo.jl/blob/gh-pages/dev/final_site/notebooks/1_how_to_run_a_model.ipynb" target="_blank" rel="noreferrer">Jupyter Notebook</a></p><hr><p><em>This page was generated using <a href="https://github.com/fredrikekre/Literate.jl" target="_blank" rel="noreferrer">Literate.jl</a>.</em></p>`,5)]))}const d=n(o,[["render",p]]);export{m as __pageData,d as default};
