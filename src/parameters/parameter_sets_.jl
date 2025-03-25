b74c9323ca70d579c255578a71b1ee595f19bb68
Eibar Joel Flores Cedeño <eibar.flores.cedeno@sintef.no>
2025-03-25 15:26:18 +0100

    dictionary like access to parameters

--- src/parameters/parameter_sets_.jl
+++ src/parameters/parameter_sets_.jl
@@ -7,39 +7,50 @@
 "Abstract type for all parameter sets in BattMo"
 abstract type ParameterSet end
 
+
+"""
+Enable Dictionary-like access to subtypes of ParameterSet.
+
+Example use: 
+
+parameters = ParameterSetSubtype(dict_with_params)
+parameters["key"] # output: value of key
+parameters["key", "subkey", "subsubkey"] # output: value of subsubkey (nested key)
+"""
+function Base.getindex(ps::ParameterSet, keys::Vararg{String})
+    value = ps.dict  
+    for key in keys
+        value = value isa Dict ? get(value, key, nothing) : nothing
+        if value === nothing
+            error("Parameter not found: " * join(keys, " -> "))
+        end
+    end
+    return value
+end
+
+
 "Cell parameter set type that represents the BattMo formatted cell parameters"
 struct CellParameters <: ParameterSet
 	dict::Dict{String, Any}
-
-	function CellParameters(file_path)
-		cell_parameters = Dict(json.load(file_path))
-		return new{typeof(cell_parameters)}(cell_parameters)
-	end
 end
 
 
 "Parameter set type that represents the cycling related parameters"
 struct CyclingProtocol <: ParameterSet
 	dict::Dict{String, Any}
-
-    function CyclingProtocol(file_path)
-		cell_parameters = Dict(json.load(file_path))
-		return new{typeof(cell_parameters)}(cell_parameters)
-	end
 end
 
 
 "Parameter set type that represents the model related parameters"
 struct SimulationSettings <: ParameterSet
 	dict::Dict{String, Any}
-
-    function SimulationSettings(file_path)
-		cell_parameters = Dict(json.load(file_path))
-		return new{typeof(cell_parameters)}(cell_parameters)
-	end
 end
 
 
 #########################################
 # Populate Parameter sets
 #########################################
+
+function load_cell_parameters(filepath::String)
+    print("Beer is life")
+end
