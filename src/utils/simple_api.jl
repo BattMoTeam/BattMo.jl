54e9a973c722851f1203979ead94fe29f4c7c230
Francesca Watson <francesca.watson@sintef.no>
2025-04-09 11:19:15 +0200

    Add function to generate single simple output structure

--- src/utils/simple_api.jl
+++ src/utils/simple_api.jl
@@ -1,23 +1,35 @@
-export extract_time_series
+export extract_time_series_data
+export extract_output_times
 export get_simple_coords
 export extract_spatial_data
 export get_model_coords
 export get_all_coords
+export get_simple_output
 
-function extract_time_series(output::NamedTuple{(:states, :cellSpecifications, :reports, :inputparams, :extra)})
+using Jutul
+
+function extract_time_series_data(output::NamedTuple{(:states, :cellSpecifications, :reports, :inputparams, :extra)})
     
     states = output[:states]
     
-    t = [state[:Control][:ControllerCV].time for state in states]
+
     E = [state[:Control][:Phi][1] for state in states]
     I = [state[:Control][:Current][1] for state in states]
-    
-    time_series = Dict("time" => t, "voltage" => E, "current" => I)
-    
-    return time_series
+
+    #time_series_data = Dict{String, Vector{Float64}}("voltage" => E, "current" => I)
+
+    return (voltage = E, current = I)
 
 end
 
+function extract_output_times(output::NamedTuple{(:states, :cellSpecifications, :reports, :inputparams, :extra)})
+    
+    states = output[:states]
+    t = [state[:Control][:ControllerCV].time for state in states]
+
+    return (time = t)
+
+end
 
 function get_model_coords(model_part::SimulationModel)
     # Get the grid wrap for the model part
@@ -54,9 +66,25 @@ end
 function extract_spatial_data(output::NamedTuple{(:states, :cellSpecifications, :reports, :inputparams, :extra)})
     
     states = output[:states]
-    x = [state[:]]
-    return spatial
+    
+    return states
     
 end
 
+function get_simple_output(output::NamedTuple{(:states, :cellSpecifications, :reports, :inputparams, :extra)})
+    
+    # Extract the time series data
+    time_series = extract_time_series_data(output)
 
+    # Extract output times
+    output_times = extract_output_times(output)
+    
+    # Extract the model coordinates
+    model_coords = get_all_coords(output[:extra][:model])
+    
+    # Extract the spatial data
+    spatial_data = extract_spatial_data(output)
+    
+    return (time = output_times, coords = model_coords, time_series = time_series, spatial_data = spatial_data)
+
+end
