36e94d59663cd0c383fc8924c3c72ea96520a88d
eibar-flores <eibar.flores.cedeno@sintef.no>
2025-04-09 14:49:10 +0200

    updated tutorial to handle cycling protocols

--- examples/beginner_tutorials/03_handle_cycling_protocols.jl
+++ examples/beginner_tutorials/03_handle_cycling_protocols.jl
@@ -18,26 +18,42 @@ cc_discharge_protocol = read_cycling_protocol(cycling_path)
 # Load default model
 model = LithiumIonBatteryModel()
 
-# ### Modify the Cycling Protocol – Varying the C-Rate
+# ### Handle, access and edit cycling protocols
+# We manipulate the cycling protocol object in the same was as we do cell parameters in the previous tutorial. List all outermost keys:
+keys(cc_discharge_protocol)
 
-# We can have a look at the content of the CylingProtocol object:
+# Show all keys and values
 cc_discharge_protocol.all
 
+# Search for a specific parameter
+search_parameter(cc_discharge_protocol, "rate")
+
+# Access a specific parameter
+cc_discharge_protocol["DRate"]
+
+# We also change protocol parameters as dicitonaries
+cc_discharge_protocol["DRate"] = 2.0
+
+# ### Compare cell performance across DRates
+# Lets now do something more fun. Since we can edit scalar valued parameters with dictionary notation, we can loop through different DRates and run
+# a simulation for each. We can then compare the cell voltage profiles for each DRate.
+
 # Let’s define the range of C-rates to explore:
-c_rates = [0.2, 0.5, 1.0, 2.0]
+d_rates = [0.2, 0.5, 1.0, 2.0]
 
 # Now loop through these values, update the protocol, and store the results:
 outputs = []
 
-for c_rate in c_rates
+for d_rate in d_rates
 	protocol = deepcopy(cc_discharge_protocol)
-	protocol["DRate"] = c_rate
+	protocol["DRate"] = d_rate
 
 	sim = Simulation(model, cell_parameters, protocol)
 	output = solve(sim; config_kwargs = (; end_report = false))
-	push!(outputs, (c_rate = c_rate, output = output))
+	push!(outputs, (d_rate = d_rate, output = output))
 end
 nothing # hide
+
 # ### Analyze Voltage and Capacity
 # We'll extract the voltage vs. time and delivered capacity for each C-rate:
 
@@ -51,10 +67,12 @@ for result in outputs
 	E = [state[:Control][:Phi][1] for state in states]
 	I = [state[:Control][:Current][1] for state in states]
 
-	label_str = @sprintf("%.1fC", result.c_rate)
+	label_str = @sprintf("%.1fC", result.d_rate)
 	lines!(ax1, t, E, label = label_str)
 
 end
 
 fig[1, 3] = Legend(fig, ax1, "C-rates", framevisible = false)
 fig
+
+# We see this cell has poor power capabilities since its capacity decreases quite rapidly with DRate.
