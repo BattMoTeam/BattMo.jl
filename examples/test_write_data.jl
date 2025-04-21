98e8a5288fd7d7e6a9b8bdb070b5ad38a70830c7
LorenaH84 <lorenahendrix163@gmail.com>
2025-04-21 16:22:48 +0200

    small change in Xu2015 cell parameter set

--- examples/test_write_data.jl
+++ examples/test_write_data.jl
@@ -1,13 +1,13 @@
 using BattMo
 
 model_settings = load_model_settings(; from_default_set = "P2D")
-model_settings["UseSEIModel"] = "Bolay"
-model_settings["UseThermalModel"] = "Sequential"
-model_settings.all
+cell_parameters = load_cell_parameters(; from_default_set = "Xu2015")
+cycling_protocol = load_cycling_protocol(; from_default_set = "CCDischarge")
 
 model = LithiumIonBatteryModel(; model_settings);
 
-empty_cell_parameters = load_cell_parameters(; from_model_template = model);
 
-fn = "/home/lhendrix/repositories/BattMo.jl/src/input/defaults/cell_parameters/Xu2015.json"
-write_to_json_file(fn, empty_cell_parameters)
+
+sim = Simulation(model, cell_parameters, cycling_protocol);
+
+output = solve(sim; accept_invalid = true)
