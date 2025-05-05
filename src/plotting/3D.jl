5bcb27c3e8a809b851df8719b46ee7e22105f79a
LorenaH84 <lorenahendrix163@gmail.com>
2025-05-05 18:13:06 +0200

    added shift argument to plot_3D_results

--- src/plotting/3D.jl
+++ src/plotting/3D.jl
@@ -1,10 +1,31 @@
 export plot_3D_results
 
-function plot_3D_results(results::NamedTuple; colormap = :curl)
+function plot_3D_results(results::NamedTuple; shift = nothing, colormap = :curl)
 
+	if isnothing(shift)
+		shift_copy = Dict()
+	else
+		shift_copy = Dict()
+
+		if haskey(shift, "NegativeActiveMaterial")
+
+			shift_copy[:NAM] = shift["NegativeActiveMaterial"]
+		end
+		if haskey(shift, "PositiveActiveMaterial")
+			shift_copy[:PAM] = shift["PositiveActiveMaterial"]
+		end
+		if haskey(shift, "CurrentCollector")
+			shift_copy[:CC] = shift["CurrentCollector"]
+		end
+		if haskey(shift, "Electrolyte")
+			shift_copy[:Elyte] = shift["Electrolyte"]
+		end
+
+	end
+	shift_copy[:PP] = shift["NegativeActiveMaterial"]
 	states = results[:states]
 	solved_model = results[:extra][:model]
 
 
-	plot_multimodel_interactive(solved_model, states; colormap = colormap)
+	plot_multimodel_interactive(solved_model, states; shift = shift_copy, colormap = colormap)
 end
