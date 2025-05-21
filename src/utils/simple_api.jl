0d354f5f4c148e6f5f6ba3d8ffdbbf8070057d29
Francesca Watson <francesca.watson@sintef.no>
2025-05-21 13:21:10 +0200

    revert simple_api to 5291c39

--- src/utils/simple_api.jl
+++ src/utils/simple_api.jl
@@ -1,20 +1,19 @@
 export get_output_time_series
 
 export get_simple_coords
-export extract_spatial_data, extract_output_times, extract_time_series_data
+export extract_spatial_data
 export get_model_coords
+export get_multimodel_centroids
 export get_simple_output
 
 
 function get_output_time_series(output::NamedTuple, quantities::Vector{String})
 
 	selected_pairs = []
-	available_quantities = ["Voltage", "Current", "CycleNumber"]
+	available_quantities = ["Voltage", "Current"]
 
 	voltage, current = extract_time_series_data(output)
 	time = extract_output_times(output)
-	states = output[:states]
-	cycle_number = [state[:Control][:Controller].numberOfCycles for state in states]
 
 	push!(selected_pairs, :Time => time)
 
@@ -23,8 +22,6 @@ function get_output_time_series(output::NamedTuple, quantities::Vector{String})
 			push!(selected_pairs, :Voltage => voltage)
 		elseif q == "Current"
 			push!(selected_pairs, :Current => current)
-		elseif q == "CycleNumber"
-			push!(selected_pairs, :CycleNumber => cycle_number)
 		else
 			error("Quantitiy $q is not available in this data")
 		end
@@ -33,6 +30,7 @@ function get_output_time_series(output::NamedTuple, quantities::Vector{String})
 
 	return (; selected_pairs...)
 end
+
 
 
 function extract_time_series_data(output::NamedTuple{(:states, :cellSpecifications, :reports, :inputparams, :extra)})
@@ -77,6 +75,67 @@ end
 
 
 
+function get_multimodel_centroids(model::MultiModel{:Battery})
+
+
+	# TODO get this to loop through all the models in the multi model
+	Ne_grid = physical_representation(modelz[:NeAm])[:cell_centroids]
+	Pe_grid = physical_representation(modelz[:PeAm])[:cell_centroids]
+	Elyte_grid = physical_representation(modelz[:Elyte])[:cell_centroids]
+
+	mm_grid_centroids = cat(Ne_grid, Pe_grid, Elyte_grid, dims = 2)
+	mm_grid_centroids = unique(eachcol(mm_grid_centroids)) # unique coordinates
+	mm_grid_centroids = hcat(mm_grid_centroids)
+
+
+	NeAm_map = zeros(Int, eachindex(Ne_grid[1, :]))
+	for i in eachindex(Ne_grid[1, :])  # Iterate over columns of Ne_grid
+		for j in eachindex(mm_grid_centroids)  # Iterate over columns of grd
+			if Ne_grid[:, i] == mm_grid_centroids[j]
+				NeAm_map[i] = j
+			end
+		end
+	end
+
+
+	PeAm_map = zeros(Int, eachindex(Pe_grid[1, :]))
+	for i in eachindex(Pe_grid[1, :])  # Iterate over columns of Pe_grid
+		for j in eachindex(mm_grid_centroids)  # Iterate over columns of grd
+			if Pe_grid[:, i] == mm_grid_centroids[j]
+				PeAm_map[i] = j
+			end
+		end
+	end
+
+	Elyte_map = zeros(Int, eachindex(Elyte_grid[1, :]))
+	for i in eachindex(Elyte_grid[1, :])  # Iterate over columns of Elyte_grid
+		for j in eachindex(mm_grid_centroids)  # Iterate over columns of grd
+			if Elyte_grid[:, i] == mm_grid_centroids[j]
+				Elyte_map[i] = j
+			end
+		end
+	end
+
+	mm_grid_map = Dict(:NeAm => NeAm_map, :PeAm => PeAm_map, :Elyte => Elyte_map)
+
+
+	return (mm_grid_centroids = mm_grid_centroids, mm_grid_map = mm_grid_map)
+
+end
+
+
+function extract_spatial_data(output::NamedTuple{(:states, :cellSpecifications, :reports, :inputparams, :extra)})
+
+	states = output[:states]
+	nx = output[:cellSpecifications][:nx]
+	ny = output[:cellSpecifications][:ny]
+	nz = output[:cellSpecifications][:nz]
+	nt = output[:cellSpecifications][:nt]
+	Elyte_potential = Array{Float64, 4}(undef, nx, ny, nz, nt)
+
+	return states
+
+end
 
 function get_simple_output(output::NamedTuple{(:states, :cellSpecifications, :reports, :inputparams, :extra)})
 
