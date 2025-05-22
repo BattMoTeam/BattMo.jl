3c0dad73d238875dc199eb557b04d22305a98c6c
Xavier Raynaud <xavier.raynaud@sintef.no>
2025-05-22 22:25:39 +0200

    more mesh plot in example_spiral_grid

--- examples/Experimental/example_spiral_grid.jl
+++ examples/Experimental/example_spiral_grid.jl
@@ -9,33 +9,48 @@ inputparams = read_battmo_formatted_input(joinpath(pkgdir(BattMo),
 
 grids, couplings = jelly_roll_grid(inputparams)
 
-fig, ax = plot_mesh(grids["NegativeElectrode"], color = :green)
+# fig, ax = plot_mesh(grids["Electrolyte"]; color = :green)
+# plot_mesh!(ax, grids["Electrolyte"];  cells = couplings["Electrolyte"]["NegativeElectrode"]["cells"], color = :yellow)
+# plot_mesh!(ax, grids["Electrolyte"];  cells = couplings["Electrolyte"]["PositiveElectrode"]["cells"], color = :blue)
 
-plot_mesh!(ax, grids["Separator"]; color = :yellow)
-plot_mesh!(ax, grids["PositiveElectrode"]; color = :blue)
-plot_mesh!(ax, grids["PositiveCurrentCollector"]; color = :red)
-plot_mesh!(ax, grids["NegativeCurrentCollector"]; color = :black)
+# fig, ax = plot_mesh(grids["NegativeElectrode"]; color = :green)
+# plot_mesh!(ax, grids["NegativeElectrode"];  cells = couplings["NegativeElectrode"]["Electrolyte"]["cells"], color = :yellow)
+
+# fig, ax = plot_mesh(grids["NegativeElectrode"]; color = :green)
+# plot_mesh!(ax, grids["NegativeElectrode"]; boundaryfaces  = couplings["NegativeElectrode"]["NegativeCurrentCollector"]["faces"], color = :blue)
+# plot_mesh!(ax, grids["NegativeCurrentCollector"];  color = :yellow)
+
+# fig, ax = plot_mesh(grids["NegativeCurrentCollector"]; color = :black)
+# plot_mesh!(ax,
+#            grids["NegativeCurrentCollector"];
+#            boundaryfaces  = couplings["NegativeCurrentCollector"]["NegativeElectrode"]["faces"], color = :yellow)
+# plot_mesh!(ax, grids["NegativeElectrode"];  color = :blue, alpha = 0.3)
+
+fig, ax = plot_mesh(grids["PositiveCurrentCollector"]; color = :red)
+plot_mesh!(ax,
+           grids["PositiveCurrentCollector"];
+           boundaryfaces  = couplings["PositiveCurrentCollector"]["PositiveElectrode"]["faces"], color = :yellow)
+# plot_mesh!(ax, grids["PositiveElectrode"];  color = :magenta, alpha = 0.3)
+
+
+# fig, ax = plot_mesh(grids["Electrolyte"]; color = :green)
+# plot_mesh!(ax, grids["PositiveCurrentCollector"]; color = :red)
+# plot_mesh!(ax, grids["PositiveElectrode"]; color = :magenta)
+# plot_mesh!(ax, grids["NegativeCurrentCollector"]; color = :black)
+# plot_mesh!(ax, grids["NegativeElectrode"]; color = :blue)
 
 fig
 
 if false
-    fig, ax = plot_mesh(grids["NegativeCurrentCollector"])
-
     plot_mesh!(ax,
                grids["NegativeCurrentCollector"],
                boundaryfaces = couplings["NegativeCurrentCollector"]["External"]["boundaryfaces"],
                color = :red,
                alpha = 0.5)
-
-    plot_mesh!(ax, grids["PositiveCurrentCollector"])
-
     plot_mesh!(ax,
                grids["PositiveCurrentCollector"],
                boundaryfaces = couplings["PositiveCurrentCollector"]["External"]["boundaryfaces"],
                color = :blue,
                alpha = 0.5)
-
-    # fig, ax = Jutul.plot_mesh(grids["Electrolyte"])
-    # Jutul.plot_mesh_edges!(ax, grids["Electrolyte"])
 end
 
