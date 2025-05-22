233a6bd208f4a6067480d8dc45d587595889b4d8
Xavier Raynaud <xavier.raynaud@sintef.no>
2025-05-22 20:37:51 +0200

    some clean-up in example_old_input

--- examples/Experimental/example_old_input.jl
+++ examples/Experimental/example_old_input.jl
@@ -1,12 +1,17 @@
 using BattMo
 
+####################
+# setup simulation #
+####################
+
 function getinput(name)
     return read_battmo_formatted_input(joinpath(pkgdir(BattMo), "examples", "Experimental", "jsoninputs", name))
 end
 
 # load geometry parameters
-# inputparams_geometry = getinput("4680-geometry.json")
-inputparams_geometry = getinput("geometry1d.json")
+inputparams_geometry = getinput("4680-geometry.json")
+# inputparams_geometry = getinput("geometry-1d.json")
+# inputparams_geometry = getinput("geometry-3d-demo.json")
 # load material parameters
 inputparams_material = getinput("lithium_ion_battery_nmc_graphite.json")
 # load control parameters
@@ -14,7 +19,43 @@ inputparams_control = getinput("cc_discharge_control.json")
 
 inputparams = merge_input_params([inputparams_geometry, inputparams_material, inputparams_control])
 
-inputparams["include_current_collectors"] = false
 
-run_battery(inputparams)
+##################
+# run simulation #
+##################
 
+output = run_battery(inputparams)
+
+############
+# plotting #
+############
+
+states = output[:states]
+
+t = [state[:Control][:Controller].time for state in states]
+E = [state[:Control][:Phi][1] for state in states]
+I = [state[:Control][:Current][1] for state in states]
+
+f = Figure(size = (1000, 400))
+
+ax = Axis(f[1, 1],
+	title = "Voltage",
+	xlabel = "Time / s",
+	ylabel = "Voltage / V",
+	xlabelsize = 25,
+	ylabelsize = 25,
+	xticklabelsize = 25,
+	yticklabelsize = 25,
+)
+
+scatterlines!(ax,
+	t,
+	E;
+	linewidth = 4,
+	markersize = 10,
+	marker = :cross,
+	markercolor = :black,
+	label = "Julia",
+)
+
+f
