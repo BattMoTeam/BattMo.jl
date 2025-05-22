94eb46ae1cade5ca0b2e8a2798d54874136060ba
Xavier Raynaud <xavier.raynaud@sintef.no>
2025-05-22 19:52:08 +0200

    minor changes in comments

--- examples/Experimental/test_jelly_roll_setup.jl
+++ examples/Experimental/test_jelly_roll_setup.jl
@@ -4,11 +4,11 @@ function getinput(name)
     return read_battmo_formatted_input(joinpath(pkgdir(BattMo), "examples", "Experimental", "jsoninputs", name))
 end
 
-# load geometry parameters
+# Load geometrical parameters
 inputparams_geometry = getinput("4680-geometry.json")
-# load material parameters
+# Load material parameters
 inputparams_material = getinput("lithium_ion_battery_nmc_graphite.json")
-# load control parameters
+# Load control parameters
 inputparams_control = getinput("cc_discharge_control.json")
 
 inputparams = merge_input_params([inputparams_geometry, inputparams_material, inputparams_control])
