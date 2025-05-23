70c8f8de834fbd3f03a81adb527a9200ce14741d
Olav Møyner <olav.moyner@gmail.com>
2025-05-23 13:36:22 +0200

    Start drafting the calibration interface/utilities

---
+++ src/calibration/calibration.jl
@@ -0,0 +1,63 @@
+struct VoltageCalibration
+    t
+    v
+    sim
+    parameter_targets
+    function VoltageCalibration(t, v, sim)
+        @assert length(t) == length(v)
+        for i in 2:length(t)
+            @assert t[i] > t[i - 1]
+        end
+        return new(t, v, sim, Dict{Vector{String}, Any}())
+    end
+end
+
+function free_calibration_parameter!(vc::VoltageCalibration, parameter_name::Vector{String};
+            initial_value = missing,
+            lower_bound = missing,
+            upper_bound = missing
+        )
+
+    if ismissing(lower_bound) || ismissing(upper_bound)
+        throw(ArgumentError("Bounds must be set for free parameters (defaults not implemented)"))
+    end
+    if !ismissing(initial_value)
+        set_calibration_parameter!(vc, parameter_name, initial_value)
+    end
+    initial_value = get_nested_json_value(vc.sim.cell_parameters, parameter_name)
+    if initial_value < lower_bound || initial_value > upper_bound
+        throw(ArgumentError("Initial value $initial_value out of bounds [$lower_bound, $upper_bound]"))
+    end
+    if lower_bound >= upper_bound
+        throw(ArgumentError("Lower bound $lower_bound must be less than upper bound $upper_bound"))
+    end
+    vc.parameter_targets[parameter_name] = (v0 = initial_value, vmin = lower_bound, vmax = upper_bound)
+    return vc
+end
+
+function freeze_calibration_parameter!(vc::VoltageCalibration, parameter_name::Vector{String}, val = missing)
+    if !ismissing(val)
+        set_calibration_parameter!(vc, parameter_name, val)
+    end
+    delete!(vc.parameter_targets, parameter_name)
+end
+
+function print_calibration_overview(vc::VoltageCalibration)
+    header = ["Parameter name", "Initial Value", "Lower Bound", "Upper Bound"]
+    pt = vc.parameter_targets
+    pkeys = keys(pt)
+
+    tab = Matrix{Any}(undef, length(pkeys), 4)
+    for (i, k) in enumerate(pkeys)
+        tab[i, 1] = join(k, ".")
+        tab[i, 2] = pt[k].v0
+        tab[i, 3] = pt[k].vmin
+        tab[i, 4] = pt[k].vmax
+    end
+    # TODO: Do this properly instead of via Jutul's import...
+    Jutul.PrettyTables.pretty_table(tab, header=header)
+end
+
+function set_calibration_parameter!(vc::VoltageCalibration, parameter_name::Vector{String}, value)
+    set_nested_json_value!(vc.sim.cell_parameters, parameter_name, value)
+end
