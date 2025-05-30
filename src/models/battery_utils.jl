2cddcae718b3521438af4989d7035e72d488e595
Xavier Raynaud <xavier.raynaud@sintef.no>
2025-05-30 18:21:44 +0200

    cosmetics in battery_utils

--- src/models/battery_utils.jl
+++ src/models/battery_utils.jl
@@ -52,11 +52,11 @@ end
 @inline grad(c_self, c_other, p::AbstractArray) = @inbounds (p[c_other] - p[c_self])
 
 @inline function half_face_two_point_kgrad(c_self::I,
-	c_other::I,
-	T::R,
-	phi::AbstractArray,
-	k::AbstractArray,
-) where {R <: Real, I <: Integer}
+	                                       c_other::I,
+	                                       T::R,
+	                                       phi::AbstractArray,
+	                                       k::AbstractArray,
+                                           ) where {R <: Real, I <: Integer}
 
 	k_av = harmonic_average(c_self, c_other, k)
 	grad_phi = grad(c_self, c_other, phi)
