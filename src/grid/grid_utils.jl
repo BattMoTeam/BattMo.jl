7fd13123bc50fa098ca8585a3eb98354c5fc5de4
Xavier Raynaud <xavier.raynaud@sintef.no>
2025-06-03 13:11:09 +0200

    added function to setup tabs for jellyroll

--- src/grid/grid_utils.jl
+++ src/grid/grid_utils.jl
@@ -317,6 +317,133 @@ function jelly_roll_grid(geomparams::InputGeometryParams)
     
 end
 
+""" returns the coupling cells and faces for the tabs for the given component ("NegativeCurrentCollector" or "PositiveCurrentCollector")"""
+function setup_tab_couplings(grids, inputparams, component)
+    
+    grid = grids[component]
+    geo = tpfv_geometry(grid)
+
+    # We use the vertical faces of the current collector, at the top, to compute the spiral length. We collect their indices
+    # in the structure vectbcface
+
+    c  = geo.boundary_centroids[1 : 2, :]
+    n  = geo.boundary_normals[1 : 2, :]
+    nc = [norm(col) for col in eachslice(c, dims = 2)]
+
+    vectbcface = findall(((1.0 ./nc) .* vec(sum(c .* n, dims = 1))) .< - (1 - 0.01)) 
+
+    zc = geo.boundary_centroids[3, vectbcface]
+
+    nz = inputparams["Geometry"]["numberOfDiscretizationCellsVertical"]
+
+    if component == "NegativeCurrentCollector"
+        vectbcface = vectbcface[abs.(zc .- maximum(zc)) .<= 0.01/nz*(maximum(zc) - minimum(zc))]
+    else
+        vectbcface = vectbcface[abs.(zc .- minimum(zc)) .<= 0.01/nz*(maximum(zc) - minimum(zc))]
+    end
+
+    # we sort by radius, so that the cells are ordered along the spiral in increasing order of radius
+
+    vectbcface = sort(vectbcface, by = x -> norm(geo.boundary_centroids[1 : 2, x]))
+
+    # We use the horizontal face centroids to compute the spiral length which we store in the structure spiral_lengths
+
+    c = geo.boundary_centroids[1 : 2, vectbcface]
+    dc = diff(c, dims = 2)
+    dl = [norm(col) for col in eachslice(dc, dims = 2)]
+
+    spiral_lengths = [0; cumsum(dl)]
+    spiral_length  = spiral_lengths[end]
+
+    # We recover the parameters for the tabs. The location of the tabs is given by a fraction which determines the fraction
+    # of the total spiral length where the tab is located.
+    if component == "NegativeCurrentCollector"
+        ip_component = "NegativeElectrode"
+    else
+        ip_component = "PositiveElectrode"
+    end
+    
+    tab_width     = inputparams[ip_component]["CurrentCollector"]["tabparams"]["width"]
+    tab_fractions = inputparams[ip_component]["CurrentCollector"]["tabparams"]["fractions"]
+
+    # The tab_intervals is an array of intervals given the lower and upper limit of the tab extent in term of the spiral
+    # length
+    tab_intervals  = [spiral_length*fraction .+ [-tab_width/2, tab_width/2] for fraction in tab_fractions]
+
+    # We collect the faces (still vertical faces at top of current collector) in the tab_vert_faces structure.
+    tab_vert_faces = []
+
+    for interval in tab_intervals
+        inds = findall(l -> ((l > interval[1]) && (l < interval[2])), spiral_lengths)
+        push!(tab_vert_faces, vectbcface[inds])
+    end
+
+    # We use neigborship to retrieve the current collector cells that contains the collected current collector tab vertical
+    # faces
+    tab_cells = [geo.boundary_neighbors[tabvertface] for tabvertface in tab_vert_faces]
+
+    # We retrieve the top (bottom) faces corresponding to the tab_cells and collect the indices in the structure first_tab_faces.
+    """ For a given cell, collect the face that is at the top (bottom) of the cell"""
+    function get_tab_first_horz_faces(cells)
+        im = grid.boundary_faces.cells_to_faces
+        tabfaces = []
+        for cell in cells
+            for iface in im.pos[cell] : im.pos[cell + 1] - 1
+                f = im.vals[iface]
+                n = geo.boundary_normals[:, f] 
+                if abs(n[1]) + abs(n[2]) < 1e-3*abs(n[3])
+                    push!(tabfaces, f)
+                end
+            end
+        end
+        return tabfaces
+    end
+
+    first_tab_faces = [get_tab_first_horz_faces(tab_cell) for tab_cell in tab_cells]
+
+    # The structure first_tab_faces now contains the indices of the first row of top (bottom) faces of the current
+    # collector and it remains to collect the other rows. To do so, we use the geometrical properties.
+
+    # We collect all the top (bottom) faces in the vector horzfaces
+    ns = geo.boundary_normals
+    if component == "NegativeCurrentCollector"
+        horzfaces = findall(n -> abs(n[1]) + abs(n[2]) < 0.01*n[3], eachslice(ns, dims = 2))
+    elseif component == "PositiveCurrentCollector"
+        horzfaces = findall(n -> abs(n[1]) + abs(n[2]) < -0.01*n[3], eachslice(ns, dims = 2))
+    end
+
+    # We setup angle and radius of the top (bottom) faces.
+    angle_horzfaces  = [atan(c[2], c[1]) for c in eachslice(geo.boundary_centroids[1 : 2, horzfaces], dims = 2)]
+    radius_horzfaces = [norm(c) for c in eachslice(geo.boundary_centroids[1 : 2, horzfaces], dims = 2)]
+
+    """ Given a face in the first row of current collector, we collect the other cells in the radial direction. The
+    centroids of those cells should have the same angle and a radius higher as the first row face and within the
+    thickness of the current collector """
+    function get_tab_radial_faces(first_tab_face)
+        c = geo.boundary_centroids[1 : 2, first_tab_face]
+        angle = atan(c[2], c[1])
+        radius = norm(c)
+        tabhorzfaces = []
+        for (i, (angle_topface, radius_topface)) in enumerate(zip(angle_horzfaces, radius_horzfaces))
+            if (abs(angle_topface - angle) < (0.1*2*pi/inputparams["Geometry"]["numberOfDiscretizationCellsAngular"])) && (radius_topface >= radius) && (radius_topface <= (radius + inputparams["NegativeElectrode"]["CurrentCollector"]["thickness"]))
+                push!(tabhorzfaces, i)
+            end
+        end
+        return horzfaces[tabhorzfaces]
+    end
+
+    """ For a row of top faces, we collect all the faces in the radial direction, using get_tab_radial_faces
+    """
+    function get_tab_faces(firsttabrowfaces)
+        return reduce(vcat, [get_tab_radial_faces(first_tab_face) for first_tab_face in firsttabrowfaces])
+    end
+
+    boundaryface_coupling = reduce(vcat, [get_tab_faces(first_tab_face) for first_tab_face in first_tab_faces])
+    cell_coupling = geo.boundary_neighbors[boundaryface_coupling]
+    return boundaryface_coupling, cell_coupling
+    
+end
+
 ##############################
 # one dimensional grid setup #
 ##############################
