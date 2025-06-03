a1ac38dd6d319752f9a04d148b8c5e56d92992bf
LorenaH84 <lorenahendrix163@gmail.com>
2025-06-03 20:57:53 +0200

    fix test bug

--- examples/example_run_current_function.jl
+++ examples/example_run_current_function.jl
@@ -1,6 +1,32 @@
 using BattMo, GLMakie
 
-include("example_functions/current_function.jl")
+### Create wltp function to calculate Current (WLTP data from https://github.com/JRCSTU/wltp)
+
+using CSV
+using DataFrames
+using Jutul
+
+path = joinpath(@__DIR__, "../example_data/wltp.csv")
+df = CSV.read(path, DataFrame)
+
+t = df[:, 1]
+P = df[:, 2]
+
+power_func = get_1d_interpolator(t, P, cap_endpoints = false)
+
+
+
+function current_function(time, voltage)
+
+	factor = 4000 # Tot account for the fact that we're simulating a single cell instead of a battery pack
+
+	return power_func(time) / voltage / factor
+end
+
+@eval Main current_function = $current_function
+
+
+### Run a simulation with the current function
 
 model_setup = LithiumIonBattery()
 cell_parameters = load_cell_parameters(; from_default_set = "Chen2020")
--- test/test_function_input.jl
+++ test/test_function_input.jl
@@ -4,6 +4,7 @@ using BattMo
 
 	@test begin
 
+		include("../examples/example_functions/current_function.jl")
 		model_setup = LithiumIonBattery()
 		cell_parameters = load_cell_parameters(; from_default_set = "Chen2020")
 		simulation_settings = load_simulation_settings(; from_default_set = "P2D")
