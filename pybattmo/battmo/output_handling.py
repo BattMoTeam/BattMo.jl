141ee29b6ee6a14a876f9175f33912663bd0002b
LorenaH84 <lorenahendrix163@gmail.com>
2025-09-09 16:11:39 +0200

    function to convert julia namedtuple to pandas df

--- pybattmo/battmo/output_handling.py
+++ pybattmo/battmo/output_handling.py
@@ -1,5 +1,6 @@
 from .julia_import import jl
-
+import numpy as np
+import pandas as pd
 
 def get_output_time_series(*arg, **kwargs):
     return jl.get_output_time_series(*arg, **kwargs)
@@ -15,3 +16,13 @@ def get_output_metrics(*arg, **kwargs):
 
 def print_output_overview(*arg, **kwargs):
     return jl.print_output_overview(*arg, **kwargs)
+
+
+def to_pandas(julia_data):
+    # Get field names as Python strings
+    fields = [str(f) for f in jl.keys(julia_data)]
+    # Build dictionary with NumPy arrays
+    data = {f: np.array(getattr(julia_data, f)) for f in fields}
+    # Create DataFrame
+    df = pd.DataFrame(data)
+    return df
--- pybattmo/examples/simulation.py
+++ pybattmo/examples/simulation.py
@@ -1,26 +1,20 @@
 # Partial port of https://battmoteam.github.io/BattMo.jl/dev/tutorials/2_run_a_simulation
-from battmo import (
-    load_cell_parameters,
-    load_cycling_protocol,
-    LithiumIonBattery,
-    Simulation,
-    solve,
-    print_output_overview,
-    plot_interactive_3d,
-    make_interactive,
-    install_plotting,
-)
+from battmo import *
+import plotly.express as px
+import pandas as pd
+import numpy as np
 
-from ..battmo.julia_import import jl
-
-install_plotting()
 cell_parameters = load_cell_parameters(from_default_set="Chen2020")
 cycling_protocol = load_cycling_protocol(from_default_set="CCDischarge")
+
 model_setup = LithiumIonBattery()
 sim = Simulation(model_setup, cell_parameters, cycling_protocol)
 output = solve(sim)
 
 print_output_overview(output)
 
-print(type(output))
-make_interactive()
+time_series = get_output_time_series(output)
+
+df = to_pandas(time_series)
+fig = px.line(df, x="Time", y="Voltage", title="Unsorted Input")
+fig.show()
