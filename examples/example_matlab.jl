448f95fa17f628fcbfae6c9d478916a9906e4675
Xavier Raynaud <xavier.raynaud@sintef.no>
2025-10-13 17:11:01 +0200

    added example with matlab

--- examples/example_matlab.jl
+++ examples/example_matlab.jl
@@ -1,12 +1,75 @@
-# # Advanced Dict UI
+using BattMo, Jutul, GLMakie
 
-using BattMo, GLMakie
+fn = string(dirname(pathof(BattMo)), "/../test/data/matlab_files/p2d_40.mat")
+inputparams = load_matlab_input(fn)
 
-battmo_base = normpath(joinpath(pathof(BattMo) |> splitdir |> first, ".."))
-file_path = joinpath(battmo_base, "test/data/matlab_files/p2d_40.mat")
+output = BattMo.get_simulation_input(inputparams::MatlabInput)
 
-simulation_input = load_matlab_input(file_path)
+simulator  = output[:simulator]
+model      = output[:model]
+parameters = output[:parameters]
+state0     = output[:state0]
+timesteps  = output[:timesteps]
+forces     = output[:forces]
 
-output = run_simulation(simulation_input)
+# Setup solver configuration
 
-plot_dashboard(output; plot_type = "contour")
+cfg = simulator_config(simulator)
+cfg[:info_level] = 10
+
+use_model_scaling = true
+if use_model_scaling
+	scalings = BattMo.get_matlab_scalings(model, parameters)
+	tol_default = 1e-5
+	for scaling in scalings
+		model_label = scaling[:model_label]
+		equation_label = scaling[:equation_label]
+		value = scaling[:value]
+		cfg[:tolerances][model_label][equation_label] = value * tol_default
+	end
+else
+	for key in submodels_symbols(model)
+		cfg[:tolerances][key][:default] = 1e-5
+	end
+end
+
+states, = simulate(state0, simulator, timesteps; forces = forces, config = cfg)
+
+t = [state[:Control][:Controller].time for state in states]
+E = [state[:Control][:ElectricPotential][1] for state in states]
+
+f = Figure(size = (1000, 400))
+
+ax = Axis(f[1, 1],
+	title = "Voltage",
+	xlabel = "Time / s",
+	ylabel = "Voltage / V",
+	xlabelsize = 25,
+	ylabelsize = 25,
+	xticklabelsize = 25,
+	yticklabelsize = 25,
+)
+
+f = Figure(size = (1000, 400))
+
+ax = Axis(f[1, 1],
+	title = "Voltage",
+	xlabel = "Time / s",
+	ylabel = "Voltage / V",
+	xlabelsize = 25,
+	ylabelsize = 25,
+	xticklabelsize = 25,
+	yticklabelsize = 25,
+)
+
+scatterlines!(ax,
+	t,
+	E;
+	linewidth = 4,
+	markersize = 10,
+	marker = :cross,
+	markercolor = :black,
+)
+
+f
+
