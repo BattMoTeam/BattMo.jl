92d3c3c5670f246bbf97e1a6f2f141ad41ea285a
August Johansson <august.johansson@sintef.no>
2026-03-24 16:10:08 +0100

    Expose state0 and timesteps for direct input by the user

--- src/models/full_battery_models/intercalation_battery.jl
+++ src/models/full_battery_models/intercalation_battery.jl
@@ -970,6 +970,10 @@ end
 
 function setup_initial_state(input, model::IntercalationBattery)
 
+    if !isnothing(model.initial_state)
+        return model.initial_state
+    end
+
     multimodel = model.multimodel
 
     include_cc = haskey(model.settings, "CurrentCollectors")
